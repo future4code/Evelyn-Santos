243fd65b24490c71001b5b33fe96fee6f175584e
evelynoliv <evelynolives@gmail.com>
2021-10-14 20:44:52 -0300

    lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,35 @@
+// EXERCÍCIOS INTERPRETAÇÃO DE CÓDIGO
+//1- 10 / adiciona 1 até chegar a 5
+// 
+//2-A) 10 19 21 23 25 27 30
+// B) 
+//3- ****
+//
+// EXCERCÍCIO DE ESCRITA DE CÓDIGO
+// //
+// // 1-A)
+// let quantosPets=Number(prompt("Quantos pets você tem?"))
+// let nomePets=[]
+// if (quantosPets===0){
+//     console.log("Que pena! Você pode adotar um pet!")}
+// else {
+//     let nome=""    
+//     for(let i=0; i<quantosPets;i++){
+//             nome=prompt("Fale o nome dos seus pets") 
+//             nomePets.push(nome)  
+//         }
+//         for (const pets of nomePets) {
+//         console.log(pets)    
+//         }
+//   }
+// //2)
+// const arrayOriginal=[]
+// const valorMaximo=[]
+// const valorMinimo=[]
+
+
+// function imprimirArray()={
+
+// }
+
+
