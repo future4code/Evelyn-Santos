67a65d2b9399e69511cb13c12211b1b78cbebd9d
evelynoliv <evelynolives@gmail.com>
2021-11-04 20:43:14 -0300

    estados-insta4

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'florzinha'}
+          fotoUsuario={'https://i.postimg.cc/LXrdm2PR/florzinha.png'}
+          fotoPost={'https://picsum.photos/201/150'}
+        />
+      
+        <Post
+        nomeUsuario={'lindinha'}
+        fotoUsuario={'https://i.postimg.cc/BvnfghsW/lindinha.png'}
+        fotoPost={'https://picsum.photos/202/150'}
+        />
+
+
+        <Post
+          nomeUsuario={'docinho'}
+          fotoUsuario={'https://i.postimg.cc/gJwF9nnN/docinha.png'}
+          fotoPost={'https://picsum.photos/203/150'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
