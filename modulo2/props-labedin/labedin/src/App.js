245f48198f0cb6521d912fdc3f1802a71aa63034
evelynoliv <evelynolives@gmail.com>
2021-11-04 12:32:59 -0300

    changes

--- modulo2/props-labedin/labedin/src/App.js
+++ modulo2/props-labedin/labedin/src/App.js
@@ -1,80 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
-;
-
-const ContainerGrande=styled.div`
-display: flex;
-align-items: center;
-flex-direction: column;
-align-items: center;
-margin-bottom: 10px;
-justify-items: flex-start;
-`
+import * as S from './App-Styled';
 
 
-function App() {
+
+const App=()=> {
   return (
-    <div className="App">
-      <ContainerGrande>
-        <h2>Dados pessoais</h2>
-        <CardGrande 
-          imagem="https://i.postimg.cc/wj7h9Qs2/foto.jpg" 
-          nome="Evelyn Oliveira" 
-          descricao="Olá, eu sou a Evelyn, estudante da Labenu e uma entusiasta em programação!"
-        />
-        
-        <ImagemButton 
-          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
-          texto="Ver mais"
-        />
-      </ContainerGrande>
+      <S.ContainerGrande>
+        <h2>Dados Pessoais</h2>
+        <S.CardGrande>
+        <S.DivGrande> 
+             <S.ImgTitle src="https://i.postimg.cc/wj7h9Qs2/foto.jpg" alt="ProfilePic"></S.ImgTitle>
+          <p>Meu nome é Evelyn.</p>
+        </S.DivGrande>  
 
-      
-        
-        {/* <ContainerPequeno>
-          imagem="https://i.postimg.cc/FHznw6gH/streamline-icon-email-action-unread-48x48.png" 
-          nome=" E-mail" 
-          descricao="e-mail@e-mail.com"
-           
 
+        </S.CardGrande>
+       
           
-          imagem="https://i.postimg.cc/Bv8kPzXV/streamline-icon-pin-48x48.png" 
-          nome="Endereço" 
-          descricao="Rua Numero 2"
-          
+      </S.ContainerGrande>
 
 
-      </ContainerPequeno> */}
-      
-
-      <ContainerGrande>
-        <h2>Experiências profissionais</h2>
-        <CardGrande 
-          imagem="https://i.postimg.cc/FsZVGKjt/girl-working-on-computer.png" 
-          nome="NOSSO IPANEMA" 
-          descricao="Gerente Admnistrativo e Financeiro" 
-        />
-        
-        <CardGrande 
-          imagem="https://i.postimg.cc/FsZVGKjt/girl-working-on-computer.png" 
-          nome="MPGP HOLDING" 
-          descricao="Controladora" 
-        />
-      </ContainerGrande>
-
-      <ContainerGrande>
-        <h2>Minhas redes sociais</h2>
-        <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
-          texto="Facebook" 
-        />        
-
-        <ImagemButton 
-          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
-          texto="Twitter" 
-        />        
-      </ContainerGrande>
-    </div>
   );
 }
 
