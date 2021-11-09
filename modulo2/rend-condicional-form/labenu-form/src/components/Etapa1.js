5afa579d431080c7ae088b91e398eecb2a751ad1
evelynoliv <evelynolives@gmail.com>
2021-11-08 22:37:56 -0300

    rend-condicionais

---
+++ modulo2/rend-condicional-form/labenu-form/src/components/Etapa1.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default class Etapa1 extends React.Component{
+    opcaoSelect = (event) =>{
+    }
+    render(){
+
+        return (
+            <div>
+                <h2> Etapa 1 Dados Gerais</h2>
+                <p>1. Qual seu nome?</p>
+                {/* </input> */}
+                <p>2. Qual sua idade?</p>
+                {/* </input> */}
+                <p>Qual sua escolaridade?</p>
+                
+
+
+
+
+
+            </div>
+        )
+    }
+
+
+
+
+
+}
