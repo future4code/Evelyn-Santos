4ccf1a2a4ab4b99ed8da2641405382b6d60e16da
evelynoliv <evelynolives@gmail.com>
2021-12-02 20:28:01 -0300

    verificado se existem duas versoes de react

--- modulo3/projeto-astromatch-final/astromatch/src/components/MatchesPage.js
+++ modulo3/projeto-astromatch-final/astromatch/src/components/MatchesPage.js
@@ -8,6 +8,25 @@ function MatchesPage () {
     const [photo, setPhoto] = useState ({})
     const [name, setName] = useState ({})
 
+    const getMatches = () => {
+        console.log("entrou")
+        const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:evelyn/matches"
+        axios.get(url)
+        .then((resposta)=> {
+            console.log("entrou no then")
+            const newMatch = resposta.data.matches
+            setListMatches(newMatch)
+            setProfile(resposta.data.profile)
+            setPhoto(resposta.data.profile.photo)
+            setName(resposta.data.profile.name)
+            
+        })
+        .catch ((error)=> {
+            console.log("entrou no catch")
+        })
+        console.log("saiu")
+    }
+
 
 
 
